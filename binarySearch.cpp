198d02e4426f9d003a21011e5189067a1d718c6a
NurulNajatMony <nurulmony505@gmail.com>
2025-05-07 20:31:47 +0700

    menambahkan fungsi bubbleSortArray

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -31,4 +31,24 @@ void input()
         cin >> elemen[i];
     }
 }
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+        pass++;
+    }while (pass <= nPanjang - 1);
+}
+
+
     
